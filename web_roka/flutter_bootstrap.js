7489796453dc25e05de47db6b08bd1febea4d1c1
ericksonomi <efernandez@outlook.com>
2025-12-04 21:30:56 -0400

    ..

--- web_roka/flutter_bootstrap.js
+++ web_roka/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "937915243"
+    serviceWorkerVersion: "1545325056"
   }
 });
