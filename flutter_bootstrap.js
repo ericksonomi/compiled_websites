4b43f5147d4dc3dabf8c8ac3535c811a8a6d2e8a
ericksonomi <efernandez@outlook.com>
2026-06-25 09:13:32 -0400

    duncion de imprimir y reimprimir tickets

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2009241304"
+    serviceWorkerVersion: "3505234279"
   }
 });
