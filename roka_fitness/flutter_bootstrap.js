b809751462b2730b11e12bda6700855c91573d17
ericksnomi <erickson.fernandez.gracia@gmail.com>
2026-07-29 18:28:27 -0400

    ..

--- roka_fitness/flutter_bootstrap.js
+++ roka_fitness/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2491985718" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "98376422" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
